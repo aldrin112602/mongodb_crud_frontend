e0f315995a8cc524e4983a36a4cd9cdffc74a7de
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-04-22 21:52:08 +0800

    Changes:  M src/components/navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,7 +12,7 @@ const Navbar: React.FC<NavBarInterfase> = (props) => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <div className="collapse navbar-collapse justify-content-end p-3" id="navbarSupportedContent">
                         <ul className="navbar-nav mb-2 mb-lg-0">
 
                             {
